b2dad09fb505b55b0241b6a7cfdc290943fa4178
ManoMylv-tBC <93531417+ManoMylv-tBC@users.noreply.github.com>
2022-03-16 16:14:04 +0000

    Create button component and add to the header

---
+++ src/components/Button/Button.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = ({ buttonLabel }) => {
+  return (
+    <button>
+      {buttonLabel}
+    </button>
+  )
+}
+
+export default Button
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Header.css';
+import Button from '../Button/Button'
 
 const Header = () => {
   return (
     <header className="App-header">
-      My contacts book
+      <h1>My contacts book</h1>
+      <Button buttonLabel={'Sort A-Z'} />
     </header>
   )
 }
