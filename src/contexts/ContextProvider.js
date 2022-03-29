649f80c099b7c22c8429b9b47a1f1dd77666dcdf
TbcProjects <daniel@thebikeclub.co.uk>
2022-03-29 17:50:53 +0100

    adds context provider

---
+++ src/contexts/ContextProvider.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect, createContext } from "react";
+
+export const DataContext = createContext({});
+
+export const ContextProvider = ({ children }) => {
+  const [data, setData] = useState([]);
+  const [toggle, setToggle] = useState(true);
+  const [darkTheme, setDarkTheme] = useState(false);
+  const [modalContent, setModalContent] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+
+  // fetch and set data on mount
+  useEffect(() => {
+    const getData = async () => {
+      const data = await fetch("./contact-data.json");
+      const json = await data.json();
+      setData(json);
+    };
+
+    getData();
+  }, []);
+
+  const getModalContent = id => {
+    setModalContent(data.filter(profile => profile.id === id));
+  };
+
+  const toggleSort = () => {
+    if (toggle) {
+      data.sort((a, b) => (a.first_name > b.first_name ? 1 : -1));
+    } else if (!toggle) {
+      data.sort(() => 0.5 - Math.random());
+    }
+    return data;
+  };
+
+  // toggle between alpha and random order
+  const handleSort = () => {
+    setToggle(!toggle);
+    setData([...toggleSort()]);
+    console.log(toggle);
+  };
+
+  //   dark theme switcher
+  const handleTheme = () => {
+    setDarkTheme(prevState => !prevState);
+    localStorage.setItem("dark", JSON.stringify(darkTheme));
+  };
+
+  const handleModal = () => {
+    setIsOpen(prevState => !prevState);
+  };
+
+  return (
+    <DataContext.Provider
+      value={{
+        data,
+        toggle,
+        isOpen,
+        darkTheme,
+        modalContent,
+        getModalContent,
+        handleSort,
+        handleTheme,
+        handleModal,
+      }}
+    >
+      {children}
+    </DataContext.Provider>
+  );
+};
+
+export default ContextProvider;
