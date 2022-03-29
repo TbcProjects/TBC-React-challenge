aff8d58383deb108cb18fe7e595e2141b57fa5c5
TbcProjects <daniel@thebikeclub.co.uk>
2022-03-29 19:12:19 +0100

    adds variant styles when modal is open

--- src/components/ContactCard/ContactCard.js
+++ src/components/ContactCard/ContactCard.js
@@ -3,7 +3,7 @@ import "./ContactCard.css";
 import { useContext } from "react";
 import { DataContext } from "../../contexts/ContextProvider";
 
-const ContactCard = ({ data }) => {
+const ContactCard = ({ data, variant }) => {
   const context = useContext(DataContext);
   const { id, first_name, last_name, phone, profile_img } = data;
   const { darkTheme, getModalContent, handleModal } = context;
@@ -15,14 +15,20 @@ const ContactCard = ({ data }) => {
   return (
     <div
       tabIndex={"1"}
-      className={`card ${darkTheme ? "card-dark" : ""}`}
+      className={`card ${darkTheme ? "card-dark" : ""} ${
+        variant === "modal" ? "card-modal" : ""
+      }`}
       onClick={e => {
         handleModal();
         getModalContent(id);
       }}
     >
       <img src={profile_img} alt="John" />
-      <div className="card-content">
+      <div
+        className={`card-content ${
+          variant === "modal" ? "card-content--modal" : ""
+        } `}
+      >
         <p>{first_name}</p>
         <p>{last_name}</p>
         <p>{phone}</p>
